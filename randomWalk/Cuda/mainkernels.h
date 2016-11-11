7df3698355c3906e077e712d231ae9ae5d3bebba
Piotr <rd226dett@gmail.com>
2016-11-11 13:02:46 +0000

    merge fix

--- randomWalk/Cuda/mainkernels.h
+++ randomWalk/Cuda/mainkernels.h
@@ -4,7 +4,7 @@
 #include "d_quadtree.h"
 #include <vector>
 
-void randomWalkCudaWrapper(int dimBlck,int dimThread,QuadTreeManager* quadTree, int RECT_ID,unsigned int *output,unsigned int randomSeed);
+void randomWalkCudaWrapper(int dimBlck,int dimThread,QuadTreeManager* quadTree, int RECT_ID,unsigned int *output,unsigned long long randomSeed);
 QuadTreeManager* createQuadTree(const std::vector<d_Rect>& layer,d_Rect const& spaceSize,bool doCheck);
 QuadTreeManager* randomWalkCudaInit(char* path);
 bool initCuda(int argc, char **argv);
